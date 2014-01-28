66c90a52466ab53c511bc6de329a4c0934b25785
Jim Rosser <jarosser06@gmail.com>
2014-01-28 14:32:45 -0600

    add tmux, remove ms.omniti publisher

--- provision.sh
+++ provision.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# install bits for packaging
 pkg install developer/versioning/git developer/gcc48 \
 developer/object-file developer/library/lint \
-developer/build/gnu-make system/header system/library/math/header-math
+developer/build/gnu-make system/header system/library/math/header-math \
+tmux
+
+# remove ms.omniti
+pkg unset-publisher ms.omniti.com
